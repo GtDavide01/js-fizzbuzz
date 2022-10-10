133211f2fbfc7a8c746da045fbafc739ee261608
GtDavide01 <davidefiorini01@gmail.com>
2022-10-10 15:40:12 +0200

    bug

--- js/script.js
+++ js/script.js
@@ -2,39 +2,28 @@
 const listNumber = document.getElementById("listnumberfizzbuzz");
 console.log(listNumber);
 
+//varibaile che contiene il messaggio 
+let stampa = "";
 //stampo in console i numeri da 1 a 100 
 for ( let i=1 ; i<= 100 ; i++){
+    
     //SE sono multipli di 3 e di 5 si chiameranno FizzBuzz 
     if (i%3===0 && i%5===0){
-        let i = "FizzBuzz"
-        console.log(i);
-         //creo variabile che contiene un elemento square 
-        const message = document.createElement("square");
-         message.innerHTML = i;
-        listNumber.append(message);
+        stampa = "FizzBuzz"
+        console.log(stampa);
+        message.classList.add("square");
     }//Altrimenti SE sono multipli di 3 si chiametaano Fizz
     else if(i%3===0){
-        let i = "Fizz";
-        console.log(i);
-         //creo variabile che contiene un elemento square 
-         const message = document.createElement("square");
-         message.innerHTML = i;
-        listNumber.append(message);
+        stampa = "Fizz";
+        console.log(stampa);
     }//Altrimenti SE sono multipli di 5 si chiameranno Buzz
     else if (i%5===0){
-        let i = "Buzz";
-        console.log(i);
-         //creo variabile che contiene un elemento square 
-         const message = document.createElement("square");
-         message.innerHTML = i;
-        listNumber.append(message);
+        stampa = "Buzz";
+        console.log(stampa);
     }//Altrimenti stampa semplicemtne il numero
     else{
+        stampa = i ; 
         console.log(i);
-         //creo variabile che contiene un elemento square 
-         const message = document.createElement("square");
-         message.innerHTML = i;
-        listNumber.append(message);
     } 
     
    
